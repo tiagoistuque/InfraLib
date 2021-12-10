16d79b6d5a216d533c81f080a02d4537d8b3778e
Tiago Istuque <tiago@tbisistemas.com.br>
2021-12-10 12:22:44 -0300

    amostrar inicial

---
+++ samples/DBConnection/DBConnection.dpr
@@ -0,0 +1,14 @@
+program DBConnection;
+
+uses
+  Vcl.Forms,
+  DBConnection.View in 'src\DBConnection.View.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
